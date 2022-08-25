03600bb98a64bee39a22daa3f65f2391ddf00ad1
Kris Magat <kmag.dev@gmail.com>
2022-08-24 19:37:55 -0700

    save

--- client/main.js
+++ client/main.js
@@ -2,27 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //get the quote from the source   
 //make a get request from https://zenquotes.io/api/random/
-// fetch('https://zenquotes.io/api/random', {
-//     mode: 'no-cors',
-//     headers: {
-//       'Access-Control-Allow-Origin':'*',
-//     credentials: 'include'
-//     }})
-// .then((data) => {
-//     console.log(data); 
-//     data.json();
-// })
-// .then((data) => {
-//   console.log(data[0].h)
-//   let quote = document.getElementById('#quote');
-//   quote.innerHTML = data[0].h;
-  
-//   });
-
-//display elements from the source into our div's
-
-
-
 
 const api_url ="https://api.goprogram.ai/inspiration";
 
